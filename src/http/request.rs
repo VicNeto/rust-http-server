58d5d67c645ac058c49b50c9913dfbeca674c5b9
VicNeto <victor.nedi@gmail.com>
2021-03-04 21:23:43 -0600

    - added: parse error struct - modified: modules split

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,51 @@
 use super::method::Method;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Display, Formatter, Debug, Result as FmtResult};
 
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
 }
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+
+    //  GET /some-path?abc=a HTTP/1.1
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!();
+    }
+}
+
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
+        }
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+impl Error for ParseError {}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,9 @@
 use server::Server;
 
+mod http;
 mod server;
 
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
-}
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-enum Method {
-    GET,
-    POST,
-    DELETE,
-    PUT,
-    PATCH,
-    HEAD,
-    OPTIONS,
-    CONNECT,
-    TRACE,
 }
