a5e4b8945f68d3de7b7c8c622a4537a95581b6fb
VicNeto <victor.nedi@gmail.com>
2021-03-06 23:33:05 -0600

    - added: response struct

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,7 +1,11 @@
+pub use query_string::{QueryString, Value as QueryStringValue};
 pub use request::Request;
 pub use method::Method;
 pub use request::ParseError;
+pub use response::Response;
 
 pub mod method;
 pub mod request;
 pub mod query_string;
+pub mod response;
+pub mod status_code;
---
+++ src/http/status_code.rs
@@ -0,0 +1,15 @@
+pub enum StatusCode {
+    Ok = 200,
+    BadRequest = 400,
+    NotFound = 404,
+}
+
+impl StatusCode {
+    pub fn reason_phrase(&self) -> &str {
+        match self {
+            Self::Ok => "Ok",
+            Self::BadRequest => "BadRequest",
+            Self::NotFound => "NotFound",
+        }
+    }
+}
